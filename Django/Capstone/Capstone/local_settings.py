d848cca28b54d390f84925943f17b332c87a9316
Joel Friedly <joelfriedly@gmail.com>
2013-02-23 00:02:20 +0000

    Deleted local_settings.py, moved DB password there

--- Django/Capstone/Capstone/local_settings.py
+++
@@ -1,3 +0,0 @@
-import settings
-
-settings.SECRET_KEY = '_o2cj6t-0jz7wg^s2wc!*t788iuj_z_5@@tx6p&amp;@bz(j1^!bdd'
--- Django/Capstone/Capstone/settings.py
+++ Django/Capstone/Capstone/settings.py
@@ -8,7 +8,7 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
-	('JC', 'crouching.jahi@gmail.com'),
+    ('JC', 'crouching.jahi@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -18,7 +18,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'genann', # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'crouching',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
